9c34c0b186f104c6153ba984df3898ef07547744
Zachary Gamble <zjgamble@mymail.mines.edu>
2020-11-13 14:28:45 -0700

    Revert ai.development.ts to previous version

--- ai/ai.development.ts
+++ ai/ai.development.ts
@@ -33,19 +33,6 @@ interface ITeamMemberState {
 
 type Coord = [number, number]
 
-function isSafeTrivial(gameState: IGameState, coord: Coord) {
-	let [row, col] = coord;
-	let tile = gameState.tileStates[row][col];
-	switch (tile) {
-	case TileState.Good:
-	case TileState.Warning:
-		return true;
-	case TileState.Danger:
-	case TileState.Broken:
-		return false;
-	}
-}
-
 function isSafe(gameState: IGameState, coord: Coord, dir: MoveDirection) {
 	let width = gameState.boardSize[0];
 	let height = gameState.boardSize[1];
@@ -86,12 +73,12 @@ function isSafe(gameState: IGameState, coord: Coord, dir: MoveDirection) {
 	}
 }
 
-function getTargetListImpl(gameState: IGameState, coord: Coord) {
+function getTargetList(gameState: IGameState, player: ITeamMemberState) {
 	let targets: MoveDirection[] = [ MoveDirection.North, MoveDirection.South, MoveDirection.East, MoveDirection.West, MoveDirection.None ];
 	let results: MoveDirection[] = [];
 
 	for (let dir of targets) {
-		if (isSafe(gameState, coord, dir)) {
+		if (isSafe(gameState, player.coord, dir)) {
 			results.push(dir);
 		}
 	}
@@ -102,117 +89,8 @@ function getTargetListImpl(gameState: IGameState, coord: Coord) {
 	return results;
 }
 
-function getTargetList(gameState: IGameState, player: ITeamMemberState) {
-	return getTargetListImpl(gameState, player.coord);
-}
-
-function getCoord(gameState: IGameState, coord: Coord, dir: MoveDirection) {
-	let width = gameState.boardSize[0];
-	let height = gameState.boardSize[1];
-
-	let [row, col] = coord;
-
-	switch (dir) {
-	case MoveDirection.North:
-		row -= 1;
-		break;
-	case MoveDirection.South:
-		row += 1;
-		break;
-	case MoveDirection.East:
-		col += 1;
-		break;
-	case MoveDirection.West:
-		col -= 1;
-		break;
-	case MoveDirection.None:
-		break;
-	default:
-		return null;
-	}
-
-	if (row >= height || row < 0 || col >= width || col < 0 ) {
-		return null;
-	}
-
-	let ret: Coord = [row, col];
-	return ret;
-}
-
-function findLongestPathImpl(gameState: IGameState, coord: Coord, visited: Coord[]) {
-	let maxScore: number = 0;
-	let targets: MoveDirection[] = getTargetListImpl(gameState, coord);
-	visited.push(coord); // constrain recursion
-
-	for (let dir of targets) {
-		if (dir === MoveDirection.None)
-			continue;
-		let newCoord = getCoord(gameState, coord, dir);	
-		if (newCoord === null || visited.indexOf(coord) !== -1)
-			continue;
-		if (!isSafeTrivial(gameState, <Coord> newCoord)) 
-			continue;
-		let score = 1; // 1 for movement
-		score += findLongestPathImpl(gameState, <Coord> newCoord, visited);
-		if (score > maxScore)
-			maxScore = score;
-	}
-
-	visited.pop();
-
-	return maxScore;
-}
-
-// return move direction with longest path
-// This does not account for jumping
-function findLongestPath(gameState: IGameState, player: ITeamMemberState) {
-	let targets: MoveDirection[] = getTargetList(gameState, player);
-	let scores: number[] = [];
-	
-	for (let dir of targets) {
-		if (dir === MoveDirection.None) {
-			scores.push(-1);
-			continue;
-		}
-		let newCoord = getCoord(gameState, player.coord, dir);	
-		if (newCoord === null) {
-			scores.push(-1);
-			continue;
-		}
-		if (!isSafeTrivial(gameState, <Coord> newCoord)) {
-			scores.push(0);
-			continue;
-		}
-		let scoreBase: number = 1; // because this means there is a movement, but it might be unsafe
-		scoreBase += findLongestPathImpl(gameState, <Coord>newCoord, []);
-		scores.push(scoreBase);
-	}
-
-	let maxScore = scores[0];
-	let index = 0;
-	for (let i = 1; i < targets.length; i++) {
-		if (scores[i] > maxScore) {
-			maxScore = scores[i];
-			index = i;
-		}
-	}
-
-	return targets[index];
-}
-
-function getNextMoveLongest(gameState: IGameState, player: ITeamMemberState) {
-	if (player.isDead)
-		return MoveDirection.None;
-	if (isSafeTrivial(gameState, player.coord)) 
-		return MoveDirection.None;
-
-	return findLongestPath(gameState, player);
-}
-
 function getNextMove(gameState: IGameState, player: ITeamMemberState) {
 	if (player.isDead)
-		return MoveDirection.None;
-	if (isSafeTrivial(gameState, player.coord)) 
 		return MoveDirection.None;
 
 	let targets = getTargetList(gameState, player);
@@ -229,8 +107,7 @@ function main(gameState: IGameState, side: Side) {
 	let moves: MoveDirection[] = [];
 	
 	for (let player of myTeam) {
-	//	moves.push(getNextMove(gameState, player));
-		moves.push(getNextMoveLongest(gameState, player));
+		moves.push(getNextMove(gameState, player));
 	}
 
 	return moves;
